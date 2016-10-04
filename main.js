375e5b6577c04cf372088b2bdb5b54631b69b044
Matias Mariani <matiasironyard@gmail.com>
2016-10-03 22:04:33 -0400

    js functioning working on css

---
+++ main.js
@@ -0,0 +1,26 @@
+(function($){
+  $(function(){
+    "use strict";
+
+var navRow = $('.nav-row');
+var menuList = $('.menu-list');
+var menuItems = $('li');
+
+
+menuItems.hide();
+
+navRow.click(function(event){
+  event.preventDefault();
+  navRow.css('background-color', '#4E5054');
+});
+
+
+navRow.click(function(event){
+  event.preventDefault();
+
+  $(this).next().children().slideToggle(500);
+  $(this).css('background-color', '#849A32');
+});
+
+});
+}(jQuery));
